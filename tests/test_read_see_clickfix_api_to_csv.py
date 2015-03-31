102eed3e930f4c1cf10d00cb36c4e7a60134537a
Matt Giguere <matthew.giguere@gmail.com>
2015-03-31 18:50:13 -0400

    added test routine
    to test read_see_clickfix_api_to_csv.py

---
+++ tests/test_read_see_clickfix_api_to_csv.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+"""
+PURPOSE: The routines in this file test the read_see_clickfix_api_to_csv module.
+
+Created on 2015-03-31T18:47:31
+"""
+
+from __future__ import division, print_function
+import numpy as np
+from types import *
+from nose.tools import raises
+import pandas as pd
+
+__author__ = "Matt Giguere (github: @mattgiguere)"
+__license__ = "MIT"
+__version__ = '0.0.1'
+__maintainer__ = "Matt Giguere"
+__email__ = "matthew.giguere@yale.edu"
+__status__ = " Development NOT(Prototype or Production)"
+
+
+def test_test_read_see_clickfix_api_to_csv():
+    """
+    Ensure
+    """
+
+#@raises(ValueError)
+#def test_make_function_raise_value_error():
+
+
