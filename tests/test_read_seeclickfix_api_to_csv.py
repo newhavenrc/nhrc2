b7c2b66adf76be6f303f0726441de20d0a79cd33
Matt Giguere <matthew.giguere@gmail.com>
2015-03-31 19:26:05 -0400

    renamed the test file
    to be consistent with the name of the routine it is testing. I also
    added another test.

--- tests/test_read_seeclickfix_api_to_csv.py
+++ tests/test_read_seeclickfix_api_to_csv.py
@@ -12,6 +12,8 @@ from types import *
 from nose.tools import raises
 import pandas as pd
 
+from nhrc2.backend import read_seeclickfix_api_to_csv as rscf
+
 __author__ = "Matt Giguere (github: @mattgiguere)"
 __license__ = "MIT"
 __version__ = '0.0.1'
@@ -20,10 +22,22 @@ __email__ = "matthew.giguere@yale.edu"
 __status__ = " Development NOT(Prototype or Production)"
 
 
-def test_test_read_see_clickfix_api_to_csv():
+def test_category_length():
     """
-    Ensure
+    Ensure there are at least 24 categories (New Haven may add more
+    at a later date, hence the inequality)
     """
+    cats = rscf.read_categories(readfile=True)
+    assert len(cats) >= 24
+
+
+
+def test_category_type():
+    """
+    Ensure that the 'type' column exists in the DataFrame
+    """
+    cats = rscf.read_categories(readfile=True)
+    assert 'type' in cats.columns.values
 
 #@raises(ValueError)
 #def test_make_function_raise_value_error():
