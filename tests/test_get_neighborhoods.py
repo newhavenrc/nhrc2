9bf8b88c32e21e37144538e0532d0c18d16ed5b3
Matt Giguere <matthew.giguere@gmail.com>
2015-04-02 21:36:32 -0400

    test function for hoods

---
+++ tests/test_get_neighborhoods.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+"""
+PURPOSE: The routines in this file test the get_neighborhoods module.
+
+Created on 2015-04-02T21:24:17
+"""
+
+from __future__ import division, print_function
+import numpy as np
+from types import *
+from nose.tools import raises
+import pandas as pd
+
+__author__ = "Matt Giguere (github: @mattgiguere)"
+__license__ = "MIT"
+__version__ = '0.0.1'
+__maintainer__ = "Matt Giguere"
+__email__ = "matthew.giguere@yale.edu"
+__status__ = " Development NOT(Prototype or Production)"
+
+
+#make sure the number of neighborhoods is equal to the number of issues.
+def test_get_neighborhoods():
+    """
+    Ensure
+    """
+
+#@raises(ValueError)
+#def test_make_function_raise_value_error():
+
+
